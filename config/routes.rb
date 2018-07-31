54ae58e5a3e5763adbc3bd8c653927f31b299cd0
eans25 <eangly.eng@gmail.com>
2018-08-01 00:56:02 +0200

    front modif2

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,10 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/about', to:'static_pages#about'
 
-  get '/users_controller', to: 'users#new', as:'about_me'
+  get '/users_controller', to: 'users#new', as:'formulaire'
   post '/users_controller', to: 'users#new_user'
 
   get '/user/:username', to: 'users#show'
   get '/error', to: 'users#error'
 
-
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
